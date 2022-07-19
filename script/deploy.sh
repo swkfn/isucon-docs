35a255a1892ffc2858381fb3a598b5d082192dc9
Yoshito Atsuta <sw.kfn4410@gmail.com>
2022-07-19 20:19:46 +0900

    fix

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 # ref: https://github.com/k-nasa/isucon-magic-powder/blob/master/deploy
-warn() {
-  printf "[\033[00;33mWARN\033[0m] $1\n"
-}
-
 BRANCH=$1
-warn "Please implement $0"
 
 echo "cd /home/isucon && \
 git stash && \
